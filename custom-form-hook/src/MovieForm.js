04c86f7b3ad65c7c634344e00089f310c75f7917
sshodges <sshodges@gmail.com>
2020-09-09 19:07:53 +1000

    use only 1 useEffect, make axios call async

--- custom-form-hook/src/MovieForm.js
+++ custom-form-hook/src/MovieForm.js
@@ -8,30 +8,24 @@ const MovieForm = () => {
 
   const [genre, GenreDropdown] = useMvsiDropdown('Genre', '', genreID);
   const [movie, MovieDropdown, setMovie] = useMvsiDropdown('Movies', '', movies);
-
+  
   useEffect(() => {
-    axios(
-      'https://api.themoviedb.org/3/genre/movie/list?api_key=f0f7cf52670158a9cb881c11bc876ddd&language=en-US'
-    )
-      .then((result) => {
-        const genreArr = result.data.genres.map((genre) => genre.id);
-        setGenreID(genreArr);
-      })
-      .catch((error) => console.log(error));
-  }, [setGenreID]);
+    // Create async function to you can use await for your axios calls
+    async function asyncGetMovieAndGenres() {
+      // Get Genres
+      let genreIds = await axios('https://api.themoviedb.org/3/genre/movie/list?api_key=f0f7cf52670158a9cb881c11bc876ddd&language=en-US');
+      genreIds = genreIds.data.genres.map((genre) => genre.id);
+      setGenreID(genreIds);
+      
+      // Get Movies
+      let moviesArr = await axios(`https://api.themoviedb.org/3/discover/movie?api_key=f0f7cf52670158a9cb881c11bc876ddd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genre}&with_original_language=en`);
+      moviesArr = moviesArr.data.results.map((movie) => movie.title);
+      setMovies(moviesArr);      
+    }
+    
+    // Call the async function
+    asyncGetMovieAndGenres();
 
-  useEffect(() => {
-    axios(
-      `https://api.themoviedb.org/3/discover/movie?api_key=f0f7cf52670158a9cb881c11bc876ddd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genre}&with_original_language=en`
-    )
-      .then((result) => {
-        const movieArr = result.data.results.map((movie) => movie.title);
-        setMovies(movieArr);
-        console.log('movies', movieArr);
-
-        console.log('%c' + movie, 'color:hotpink; font-size: 24px');
-      })
-      .catch((error) => console.log(error));
   }, [genre, movie]);
 
   return (
